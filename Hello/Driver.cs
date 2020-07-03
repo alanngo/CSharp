e3bab9bbc3529ebb15996826545108251f714eeb
alan <alan@msi.loc>
2020-07-02 19:17:24 -0500

    starting C#

---
+++ Hello/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello
+{
+    class Driver
+    {
+
+        // make it easy for printing
+        static void Log(object o) => Console.WriteLine(o);
+        static void Main(string[] args)
+        {
+            Log("Hello World!");
+        }
+    }
+}
