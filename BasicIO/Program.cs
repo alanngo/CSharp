6df97536b4c60da749594c04c0b27a13c3c54f3f
alan <alan@msi.loc>
2020-07-02 20:28:30 -0500

    basic io

---
+++ BasicIO/Program.cs
@@ -0,0 +1,28 @@
+﻿// static imports
+using static System.Console;
+using static System.Convert;
+
+
+namespace BasicIO
+{
+    class Program
+    {
+        static void Log(object o) => WriteLine(o);
+
+        static void Main(string[] args)
+        {
+            Write("Enter a string value: ");
+            string str = ReadLine();
+
+            Write("Enter an int value: ");
+            int i = ToInt32(ReadLine());
+
+            Write("Enter a double value: ");
+            double d = ToDouble(ReadLine());
+
+            Log("string: " + str);
+            Log("int: " + i);
+            Log("double: " + d);
+        }
+    }
+}
