d9babcf33f6767c9b426bbd85c7c9704a7cb1421
alan <alan@msi.loc>
2020-07-03 20:44:37 -0500

    arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        // make it easy for printing
+        static void Log(object o) => Console.WriteLine(o);
+        static void Print(int [] arr) 
+        {
+            foreach (int o in arr) //c# foreach loop
+                Log(o);
+        }
+        static void Print(string[] arr)
+        {
+            foreach (string o in arr) //c# foreach loop
+                Log(o);
+        }
+        static void Main(string[] args)
+        {
+            int[] odd = {1,3,5,7,9}; //static array
+            
+            string[] names = new string[]{ "varshika", "carlos", "smruti", "kevin" ,"lyndon"}; // dynamic array
+            Print(names);
+
+
+            Print(odd);
+        }
+    }
+}
